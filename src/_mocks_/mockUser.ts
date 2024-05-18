94a63aa223e3c75c2b9a4eba5da489ed508434b6
Pratim <hpro383@gmail.com>
2024-05-19 01:04:55 +0530

    commit

---
+++ src/_mocks_/mockUser.ts
@@ -0,0 +1,17 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'pratim',
+    displayName: 'pratim the dev',
+  },
+  {
+    id: 2,
+    username: 'ram',
+    displayName: 'ram the dev',
+  },
+  {
+    id: 3,
+    username: 'john',
+    displayName: 'john the dev',
+  },
+];
--- src/graphql/resolver/UserResolver.ts
+++ src/graphql/resolver/UserResolver.ts
@@ -1,14 +1,30 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Query,
+  ResolveField,
+  Resolver,
+  Parent,
+} from '@nestjs/graphql';
 import { User } from '../models/User';
+import { mockUsers } from 'src/_mocks_/mockUser';
+import { UserSetting } from '../models/UserSetting';
+import { mockUserSetting } from 'src/_mocks_/mockUserSettings';
 
-@Resolver()
+@Resolver(() => User)
 export class UserResolver {
-  @Query(() => User)
-  getUser() {
-    return {
-      id: 1,
-      username: 'Pratim',
-      displayName: 'Pratim',
-    };
+  @Query(() => User, { nullable: true })
+  getUserById(@Args('id', { type: () => Int }) id: number) {
+    return mockUsers.find((user) => user.id === id);
+  }
+
+  @Query(() => [User])
+  getUsers() {
+    return mockUsers;
+  }
+  @ResolveField(() => UserSetting, { name: 'settings', nullable: true })
+  getUserSetting(@Parent() user: User) {
+    console.log(user);
+    return mockUserSetting.find((setting) => setting.userId === user.id);
   }
 }
